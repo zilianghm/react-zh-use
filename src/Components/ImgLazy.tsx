73837a3e5def16cbcdf0986f379ff6f486c59ebc
曹梓梁 <zilianghm@caoziliangdeMacBook-Pro.local>
2021-12-06 20:11:28 +0800

    fix: 1.0.1

---
+++ src/Components/ImgLazy.tsx
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import {deepCompare} from '../JSUtils';
+
+interface propsType {
+    placehold?: string,
+    src?: string,
+    width?: number | string,
+    height?: number | string,
+    alt?: string,
+    crossorigin?: 'anonymous' | 'use-credentials',
+    srcset?: string,
+    sizes?: string,
+}
+
+const Img: React.FC<propsType> = (props) => {
+    const [done, setDone] = useState(false)
+    useEffect(() => {
+        const img = new Image()
+        img.src = props.src as string
+        img.onload = () => {
+            setDone(true)
+        }
+    }, [])
+
+    return (
+        <>
+            {
+                done ? <img width={props.width} height={props.height} style={{backgroundColor: '#f1f1f1'}}/> : <img {...props}/>
+            }
+        </>
+    )
+}
+
+const areEqual = (prevProps, nextProps) => {
+    return JSON.stringify(prevProps) === JSON.stringify(nextProps)
+}
+
+export const ImgLazy = React.memo(Img, areEqual)
+
