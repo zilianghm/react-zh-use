71610320cc07917473e257b5d7e54f3380f169a7
zilianghm <zilianghm@gmail.com>
2021-12-07 10:26:12 +0800

    fix: 2

--- src/components/ImgLazy/ImgLazy.tsx
+++ src/components/ImgLazy/ImgLazy.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {deepCompare} from '../JSUtils';
+import {deepCompare} from '../../jstools/tools';
 
 interface propsType {
     placehold?: string,
